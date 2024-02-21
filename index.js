1f83bc12ebc65b04e29ed40365cb0bbfd19a9e85
swagat030 <swagatkumarmishra888@gmail.com>
2024-02-21 17:08:34 +0530

    file updated

--- index.js
+++ index.js
@@ -1,19 +1,33 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 require('dotenv').config();
 require('./db');
 const PORT = process.env.PORT || 8080;
 const productRoutes = require('./routes/productRoutes');
 const userRoutes = require('./routes/userRoutes');
+const {getProducts} = require("./conrollers/productController");
+
 app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 
 app.get('/', (req, res) => {
     res.send('products api running new deploy');
 });
 
+// Error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
 app.get('/ping', (req, res) => {
     res.send('PONG')
 });
+
+app.get('/product-data', getProducts);
 // /products
 app.use('/products', productRoutes);
 // /users
