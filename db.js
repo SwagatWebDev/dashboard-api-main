5f233b6de3cefbbe6f5d6f06d51ac6812af6a50e
swagat030 <swagatkumarmishra888@gmail.com>
2024-02-22 01:18:39 +0530

    file updated

--- db.js
+++ db.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
-const dbHOST = process.env.MONGODB_LOCAL_URL;
+const dbHOST = process.env.MONGODB_DEV_URL;
 
 mongoose.connect(dbHOST)
     .then(() => {
         console.log('MongoDB Connected...')
     }).catch((err) => {
         console.log('Error while Mongo Conn..', err);
-    })
+    });
